4f6b19ba0f9e384259d42183cf82b3d0f9c6bad9
danilo-polesi <danilopolesk8@gmail.com>
2024-04-17 17:37:55 -0300

    primeiro commit

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Double n1, n2, n3;
+
+            Console.Write("Digite o 1º n.: ");
+            n1 = (Console.Read());
+            Console.Write("Digite o 2º n.: ");
+            n2 = (Console.Read());
+            Console.Write("Digite o 3º n.: ");
+            n3 = (Console.Read());
+
+            if (n1 > n2 && n1 > n3)
+            {
+                Console.WriteLine("O maior número é: " + n1);
+            }
+
+            else if (n1 > n2 && n1 < n3)
+            {
+
+            }
+
+            else if (n1 < n2 && n1 < n3)
+            {
+                Console.WriteLine("O menor número é: " + n1);
+            }
+
+            else if (n2 > n1 && n2 > n3)
+            {
+                Console.WriteLine("O maior número é: " + n2);
+            }
+
+            else if (n2 > n1 && n2 < n3)
+            {
+
+            }
+
+            else if (n2 < n1 && n2 < n3)
+            {
+                Console.WriteLine("O menor numero é: " + n2);
+            }
+
+            else if (n3 > n1 && n3 > n2)
+            {
+                Console.WriteLine("O maior número é: " + n3);
+            }
+
+            else if (n3 > n1 && n3 < n2)
+            {
+
+            }
+
+            else if (n3 < n1 && n3 < n2)
+            {
+                Console.WriteLine("O menor numero é: " + n3);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int maior = 0, menor, i = 0, Q = 0, numero = 0;
+
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.Write("Deseja informar quantos numeros?: ");
+            Q = Convert.ToInt32(Console.ReadLine());
+
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.Write("Informe o 1º número: ");
+            numero = Convert.ToInt32(Console.ReadLine());
+            maior = numero;
+            menor = numero;
+            i = 1;
+
+            for (i = 2; i <= Q; i++){
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.Write("Informe o "+i+"º número: ");
+                numero = Convert.ToInt32(Console.ReadLine());
+                if (numero < menor) { menor = numero; }
+                if (numero > maior) { maior = numero; }
+             }
+            Console.WriteLine("O maior número é: " + maior);
+            Console.WriteLine("O menor número é: " + menor);
+            Console.ReadKey();
+
+        }
+    }
+}
